fd30e41d03dc16a95e39d0f31b913c2ad0e2574e
Elias Hjelm <elias.hj@gmail.com>
2018-11-29 14:48:48 +0100

    Upgraded boilerplate

--- www/assets/components/App.js
+++ www/assets/components/App.js
@@ -2,6 +2,8 @@ class App extends Component {
  
   constructor(){
     super();
+    this.pageContent = new PageContent();
+
     new Router(this.pageContent);
     $('body').html(this.render());
   }
