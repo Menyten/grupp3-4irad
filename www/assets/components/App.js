655e5f63cab1b2a4f2bd5c895f1b17eae2e124f9
Kelvin <kelvinrylik@hotmail.com>
2018-12-03 11:25:55 +0100

    footer - a beginning

--- www/assets/components/App.js
+++ www/assets/components/App.js
@@ -3,6 +3,7 @@ class App extends Component {
   constructor(){
     super();
     this.pageContent = new PageContent();
+    this.footer = new Footer();
 
     new Router(this.pageContent);
     $('body').html(this.render());
