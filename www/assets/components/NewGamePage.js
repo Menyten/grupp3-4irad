1e561c3ada2714c65aaeb78dc2e6e4c378e3802d
Joel P <joel.pedersen97@gmail.com>
2018-12-03 16:01:59 +0100

    Added validation for input names

--- www/assets/components/NewGamePage.js
+++ www/assets/components/NewGamePage.js
@@ -2,13 +2,26 @@ class NewGamePage extends Component {
   constructor() {
     super();
     this.addRoute('/new-game', 'New Game');
-    this.addEvents({'click #players-and-opponents': 'getInput'});
+    this.addEvents({ 'click #players-and-opponents': 'getInput' });
   }
   getInput() {
-    const playerOne = $('#player-name').val();
-    const playerTwo = $('#player2-name').val();
+
+    const p1 = $('#player-name');
+    const p2 = $('#player2-name');
+    let playerOne = p1.val().length >= 2 && p1.val().length <= 10;
+    const playerTwo = p2.val().length >= 2 && p2.val().length <= 10;
     const playerOneOpponent = $('#type-of-player').val();
     const playerTwoOpponent = $('#type-of-player2').val();
+
+    if (playerOne === true) {
+      playerOne = p1.val();
+
+    }
+    if (playerTwo === true) {
+      playerOne = p2.val();
+    }
+
+
     console.log(playerOne);
     console.log(playerTwo);
     console.log(playerOneOpponent);
