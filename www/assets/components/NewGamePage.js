b82a09da9738f8843b802e5c2c01f5d79cd24046
Joel P <joel.pedersen97@gmail.com>
2018-12-03 13:21:31 +0100

    Function to save input name in variables

--- www/assets/components/NewGamePage.js
+++ www/assets/components/NewGamePage.js
@@ -2,5 +2,16 @@ class NewGamePage extends Component {
   constructor() {
     super();
     this.addRoute('/new-game', 'New Game');
+    this.addEvents({'click #players-and-opponents': 'getInput'});
+  }
+  getInput() {
+    const playerOne = $('#player-name').val();
+    const playerTwo = $('#player2-name').val();
+    const playerOneOpponent = $('#type-of-player').val();
+    const playerTwoOpponent = $('#type-of-player2').val();
+    console.log(playerOne);
+    console.log(playerTwo);
+    console.log(playerOneOpponent);
+    console.log(playerTwoOpponent);
   }
 }
