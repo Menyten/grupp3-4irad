01d5d24ad468e41ac523e57b7d499159ece995a3
Joel P <joel.pedersen97@gmail.com>
2018-12-10 14:30:31 +0100

    Added cancel(to input) button and to startpage button on game page (working)

--- www/assets/components/GamePage.js
+++ www/assets/components/GamePage.js
@@ -2,6 +2,8 @@ class GamePage extends Component {
   constructor() {
     super();
     this.addRoute('/new-game', 'New Game');
+    this.addEvents({ 'click #quit-game': 'newInput' });
+    this.addEvents({ 'click #to-startpage': 'newInput' });
     this.inputPage = new InputPage(this);
     this.game = new Board(this);
     this.activeGame = false;
@@ -13,5 +15,9 @@ class GamePage extends Component {
     this.activeGame = true;
     this.game = new Board(this).createBoard();
     this.render();
+  }
+
+  newInput() {
+    this.activeGame = false;
   }
 }
