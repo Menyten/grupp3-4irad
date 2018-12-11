57bb0625ef459c7bf01bdde9d11a810b709ff000
robinasplund <r.asplund@mail.com>
2018-12-11 13:46:32 +0100

    Switching player/markers

--- www/assets/components/Marker.js
+++ www/assets/components/Marker.js
@@ -2,6 +2,6 @@ class Marker extends Component {
   constructor(player) {
     super();    
     // we later want to replace this with either player one or two, when the game engine knows which player is playing
-    this.player = 1;
+    this.player = player;
   }  
 }
