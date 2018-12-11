be1a8a93d2020528c86d254f68fcf3b0d42f341a
Pontus Nilsson <p0nta_@hotmail.com>
2018-12-11 14:33:27 +0100

    Olika modals för olika resultat
    Tre olika funktioner till om det blir kryss,förslust eller vinst.

--- www/assets/components/Modals.js
+++ www/assets/components/Modals.js
@@ -1,28 +1,41 @@
 class Modals extends Component {
-    constructor(){
+    constructor() {
         super();
         //this.victoryModal()
-       //this.loserModal()
+        //this.loserModal()
+        //this.drawModal()
     }
 
-    show(){
+    show() {
         this.render();
-        setTimeout(()=>{
+        setTimeout(() => {
             this.baseEl.modal('show');
         }, 0);
     }
 
-    victoryModal(){
+    victoryModal() {
         this.title = "Grattis!  ";
+        $(document).ready(function () {
+            $('.modal-body').addClass('victoryImage');
+        });
         this.show();
 
     }
 
-    loserModal(){
+    loserModal() {
         this.title = "Du förlorade tyvärr";
+        $(document).ready(function () {
+            $('.modal-body').addClass('losingImage');
+        });
         this.show();
     }
 
-
+    drawModal() {
+        this.title = "Det slutade lika";
+        $(document).ready(function () {
+            $('.modal-body').addClass('drawImage');
+        });
+        this.show();
+    }
 
 }
