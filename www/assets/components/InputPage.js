d76afceef3356d01e00938e2742ea48a3d3e5291
robinasplund <r.asplund@mail.com>
2018-12-16 01:26:54 +0100

    Almost done

--- www/assets/components/InputPage.js
+++ www/assets/components/InputPage.js
@@ -3,11 +3,23 @@ class InputPage extends Component {
     super();
     this.addEvents({ 'click #start-game': 'startGame' });
     this.addEvents({'keyup .player-name': 'validateUsers'});
+
+    this.addEvents({'click .pirate1-img,.pirate2-img,.pirate3-img': 'selectPlayerType'});
+    this.playerOneType;
+    this.playerTwoType = 'human';
+
     this.playPage = playPage;
     this.playerOne;
     this.playerTwo;
     this.playersValidated = false;
   }
+
+  selectPlayerType(){
+    this.playerOneType='human';
+  }
+
+
+  
   
   getValidationStatus(){
     return this.playersValidated;
@@ -31,15 +43,18 @@ class InputPage extends Component {
     const p2 = $('#player2-name');
     let playerOne = p1.val().length >= 2 && p1.val().length <= 10;
     let playerTwo = p2.val().length >= 2 && p2.val().length <= 10;
-    const playerOneType = $('#type-of-player1').val();
-    const playerTwoType = $('#type-of-player2').val();
+    //const playerOneType = $('#type-of-player1').val();
+    //const playerTwoType = $('#type-of-player2').val();
+
+    //const playerOneType = 'computer';
+    //const playerTwoType = 'human';
+    
 
     if (playerOne === true) {
-      this.playerOne = new Player(p1.val(), playerOneType);
-
+      this.playerOne = new Player(p1.val(), this.playerOneType);
     }
     if (playerTwo === true) {
-      this.playerTwo = new Player(p2.val(), playerTwoType);
+      this.playerTwo = new Player(p2.val(), this.playerTwoType);
     }
 
   }
